250a8422dca5363cade3a0ab09e76b138824907f
Owner-PC\Owner <g@geshto.com>
2019-12-24 20:03:10 +0200

    some visual changes

--- PL/NewOrder.xaml.cs
+++ PL/NewOrder.xaml.cs
@@ -10,28 +10,31 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewOrder.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewOrder : Window
     {
-        public MainWindow()
+        public NewOrder()
         {
             InitializeComponent();
         }
 
-        private void CreateOrder_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
            
-            NewOrder orderPage = new NewOrder();
-            orderPage.Show();
+            MainWindow mw = new MainWindow();
+            mw.Show();
             this.Close();
-           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
